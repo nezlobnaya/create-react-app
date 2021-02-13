4cd3b59c5654a7753f9ef9c88c0fa2b5baf9a6ea
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-02-12 23:14:37 -0500

    updated cra template

--- packages/cra-template-astra/template/src/index.js
+++ packages/cra-template-astra/template/src/index.js
@@ -4,4 +4,11 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    <App />,
+
+  document.getElementById('root')
+);
+
+
