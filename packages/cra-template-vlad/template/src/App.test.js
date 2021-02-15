86c136f18517abf587484d4f1139db1a58aebdfa
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-02-15 15:57:37 -0500

    updated astra template and removed unnecessary repo

--- packages/cra-template-vlad/template/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders test heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/hello, world/i);
-  expect(headingElement).toBeInTheDocument();
-});
